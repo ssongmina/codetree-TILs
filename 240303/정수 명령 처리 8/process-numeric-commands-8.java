3f0659c54209b912ab40d0d1a689c7306f987c70
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-03 17:44:10 +0900

    Title: 정수 명령 처리 8, Time: 385ms, Memory: 21MB, Status: Passed - CodeTree

--- 240303/정수 명령 처리 8/process-numeric-commands-8.java
+++ 240303/정수 명령 처리 8/process-numeric-commands-8.java
@@ -1,21 +1,23 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         LinkedList<Integer> list = new LinkedList<>();
         StringBuilder s = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+        int n = sc.nextInt();
         for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String str = st.nextToken;
+            String str = sc.next();
             switch(str){
                 case "push_front":
-                    int a = Integer.parseInt(st.nextToken);
+                    int a = sc.nextInt();
                     list.addFirst(a);
                     break;
                 case "push_back":
-                    int a = Integer.parseInt(st.nextToken);
-                    list.addLast(a);
+                    int b = sc.nextInt();
+                    list.addLast(b);
                     break;
                 case "pop_front":
                     s.append(list.pollFirst()).append("\n");
@@ -32,13 +34,13 @@ public class Main {
                     else s.append("0\n");
                     break;
                 case "front":
-                    s.append(list.peekFirst());
+                    s.append(list.peekFirst()).append("\n");
                     break;
                 case "back":
-                    s.append(list.peekLast());
+                    s.append(list.peekLast()).append("\n");
                     break;
             }
         }
-        s.flush();
+        System.out.println(s);
     }
 }
