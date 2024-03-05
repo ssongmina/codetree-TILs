bc38a649a567aee7d1a528c8efd5b88121795651
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-05 20:05:58 +0900

    Title: 황금비율 토스트, Time: 1289ms, Memory: 36MB, Status: Passed - CodeTree

--- 240305/황금비율 토스트/golden-toast.java
+++ 240305/황금비율 토스트/golden-toast.java
@@ -1,49 +1,52 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
+    public static int n, m;
+    public static String s;
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String str1 = sc.next();
+
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        s = sc.next();
+
+        // 연결리스트 정의
         LinkedList<Character> l = new LinkedList<>();
-        
-
-        for(int i=0; i<str1.length(); i++){
-            l.add(str1.charAt(i));
-        }
-        
+        for(int i = 0; i < s.length(); i++)
+			l.add(s.charAt(i));
+        // Iterator 정의 (맨 끝 위치에서 시작하기 위해서는 listIterator안에 l.size() 필요)
         ListIterator<Character> it = l.listIterator(l.size());
-        for(int i=0; i<m+1; i++){
-            String[] str2 = sc.nextLine().split(" ");
-            switch(str2[0]){
-                case "L":
-                    if(it.hasPrevious())
-                        it.previous();
-                    break;
-                case "R":
-                    if(it.hasNext())
-                        it.next();
-                    break;
-                case "D":
-                    if(it.hasNext()){
-                        it.next();
-                        it.remove();
-                    }
-                    break;
-                case "P":
-                    char a = str2[1].charAt(0);
-                    it.add(a);
 
-                    break;
+        while(m-- > 0) {
+            char command = sc.next().charAt(0);
+
+            if(command == 'L') {
+                if(it.hasPrevious()) // 빵들의 맨 앞이 아니라면
+                    it.previous();   // 앞으로 이동합니다.
+            } 
+            else if(command == 'R') {
+                if(it.hasNext())     // 빵들의 맨 뒤가 아니라면
+                    it.next();       // 뒤로 이동합니다.
             }
-            
+            else if(command == 'D') {
+                if(it.hasNext()) {   // 빵들의 맨 뒤가 아니라면
+                    it.next();
+                    it.remove();     // 바로 뒤에 있는 빵을 제거합니다.
+                }
+            }
+            else if(command == 'P') {
+                char c = sc.next().charAt(0);
+                it.add(c);           // 가리키는 위치에 문자 c를 추가합니다.
+            }    
         }
+
+        // 출력:
         it = l.listIterator();
-        while(it.hasNext()){
+        while(it.hasNext())
             System.out.print(it.next());
-        }
     }
 }
