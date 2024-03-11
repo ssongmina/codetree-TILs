23c52ae750c4310eab9dd35ac1d96fae9191cdec
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-11 18:20:44 +0900

    Title: 최고의 33위치, Time: 3013ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

--- 240311/최고의 33위치/best-place-of-33.java
+++ 240311/최고의 33위치/best-place-of-33.java
@@ -2,15 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++)
-                arr[i][j] = sc.nextInt();
-        
+                arr[i][j] = Integer.parseInt(st.nextToken());
+        }
         int max = 0;
         for(int i=0; i<n-2; i++){
             for(int j=0; j<n-2; j++){
