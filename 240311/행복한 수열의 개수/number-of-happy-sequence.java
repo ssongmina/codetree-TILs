96d8fbb61396f87764d0f7054f9ce29f7ae49c4f
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-11 18:52:00 +0900

    Title: 행복한 수열의 개수, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240311/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240311/행복한 수열의 개수/number-of-happy-sequence.java
@@ -14,37 +14,38 @@ public class Main {
             for(int j=0; j<n; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
-
-        int cnt = 0;
-        for(int i=0; i<n; i++){
-            int sum = 1;
-            for(int j=0; j<n-1; j++){
-                if(arr[i][j] == arr[i][j+1])
-                    sum++;
-                else
-                    sum = 1;
-            }
-            if(arr[i][n-2] == arr[i][n-1])
-                sum++;
-            if(sum >= m)
-                cnt++;
-        }
-        for(int i=0; i<n; i++){
-            int sum = 1;
-            for(int j=0; j<n-1; j++){
-                if(arr[j][i] == arr[j+1][i])
-                    sum++;
-                else
-                    sum = 1;
-            }
-            if(arr[n-1][i] == arr[n-2][i])
-                sum++;
-            if(sum >= m)
-                cnt++;
-        }
         if(n==1 && m==1)
-            System.out.println("1");
-        else
+            System.out.println("2");
+        else{
+            int cnt = 0;
+            for(int i=0; i<n; i++){
+                int sum = 1;
+                for(int j=0; j<n-1; j++){
+                    if(arr[i][j] == arr[i][j+1])
+                        sum++;
+                    else
+                        sum = 1;
+                }
+                if(arr[i][n-2] == arr[i][n-1])
+                    sum++;
+                if(sum >= m)
+                    cnt++;
+            }
+            for(int i=0; i<n; i++){
+                int sum = 1;
+                for(int j=0; j<n-1; j++){
+                    if(arr[j][i] == arr[j+1][i])
+                        sum++;
+                    else
+                        sum = 1;
+                }
+                if(arr[n-1][i] == arr[n-2][i])
+                    sum++;
+                if(sum >= m)
+                    cnt++;
+            }
+           
             System.out.println(cnt);
+        }
     }
 }
