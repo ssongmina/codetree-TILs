d10f2c68deac71044f13f87af83a2a5eee154fa3
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-11 18:22:39 +0900

    Title: 최고의 33위치, Time: 3012ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

--- 240311/최고의 33위치/best-place-of-33.java
+++ 240311/최고의 33위치/best-place-of-33.java
@@ -2,20 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+
+    public static int[][] arr; 
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][n];
+        arr = new int[n][n];
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
         }
+
         int max = 0;
         for(int i=0; i<n-2; i++){
             for(int j=0; j<n-2; j++){
-                int cnt = sum(arr, i,j);
+                int cnt = sum(i,j);
                 if(cnt > max)
                     max = cnt;        
             }
@@ -23,7 +27,7 @@ public class Main {
         System.out.println(max);
     }
 
-    public static int sum(int[][] arr, int i, int j){
+    public static int sum(int i, int j){
         int sum = 0;
         for(int a = i; a<i+3; i++){
             for(int b = j; b<j+3; j++){
