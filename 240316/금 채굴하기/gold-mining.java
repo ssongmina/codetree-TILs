3cca9248dd7d406472e94df335b63a1c4817888b
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-16 14:37:14 +0900

    Title: 금 채굴하기, Time: 208ms, Memory: 11MB, Status: Passed - CodeTree

--- 240316/금 채굴하기/gold-mining.java
+++ 240316/금 채굴하기/gold-mining.java
@@ -1,51 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    
     public static final int MAX_NUM = 20;
-    public static int n,m;
-    public static int[][] arr = new int[MAX_NUM][MAX_NUM];
     
-    // 채굴 비용 반환
-    public static int getCost(int k){
-        return k*k + (k+1)*(k+1);
+    public static int n, m;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    
+    // 주어진 k에 대하여 마름모의 넓이를 반환합니다.
+    public static int getArea(int k) {
+        return k * k + (k+1) * (k+1); 
     }
-
-    // k에 대해 채굴 가능한 금의 개수 반환
-    public static int getNumOfGold(int row, int col, int k){
-        int cnt = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(Math.abs(row-i)+Math.abs(col-i) <= k)
-                    cnt += arr[i][j];
-            }
-        }
-        return cnt;
+    
+    // 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
+    public static int getNumOfGold(int row, int col, int k) {
+        int numOfGold = 0;
+    
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(Math.abs(row - i) + Math.abs(col - j) <= k)
+                    numOfGold += grid[i][j];
+    
+        return numOfGold;
     }
-
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int maxGold = 0;
+
+        // 입력:
         n = sc.nextInt();
         m = sc.nextInt();
-        for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-                arr[i][j] = sc.nextInt();
+        for(int row = 0; row < n; row++)
+            for(int col = 0; col < n; col++)
+                grid[row][col] = sc.nextInt();
 
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                for(int k=0; k<=2*(n-1); k++){
-                    int cnt = getNumOfGold(i,j,k);
-                    if(cnt * m >= getCost(k))
-                        maxGold = Math.max(maxGold, cnt);
+        // 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구합니다.
+        for(int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++) {
+                for(int k = 0; k <= 2 * (n-1); k++) {
+                    int numOfGold = getNumOfGold(row, col, k);
+
+                    // 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수를 저장합니다.
+                    if(numOfGold * m >= getArea(k))
+                        maxGold = Math.max(maxGold, numOfGold);
                 }
             }
         }
-        System.out.println(maxGold);
 
+        System.out.print(maxGold);
     }
 }
