7b989866d3292d5ccc1d54f426825a82b744f0e2
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-17 14:34:25 +0900

    Title: 양수 직사각형의 최대 크기, Time: 127ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240317/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+    public static int[][] arr;
+    public static int n;
+    public static int m;
+
+    public static int findMax(){
+        int max = 0;
+        for(int a=0; a<n; a++)
+            for(int b=0; b<n; b++)
+                for(int c=0; c<n; c++)
+                    for(int d=0; d<n; d++){
+                        int cnt = findMax(a,b,c,d);
+                        max = Math.max(cnt, max);
+                    }
+        return max;
+    }
+
+    public static int findMax(int a, int b, int c, int d){
+        for(int i=a; i<=c; i++){
+            for(int j=b; j<=d; j++){
+                if(arr[i][j] <=0)
+                    return -1;
+            }
+        }
+        return (c-a+1)*(d-b+1);
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n][m];
+        for(int i=0; i<n; i++)  
+            for(int j=0; j<m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        int cnt = findMax();
+        System.out.println(cnt);
+        
+        
+    }
+}
