10dc7101ee920905ac634303fc03bb4209e307f5
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-17 14:05:52 +0900

    Title: 반복 출력하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240317/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void line(){
+        System.out.println("12345^&*()_");
+    }
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+            line();
+    }
+}
