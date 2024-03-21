ebe44574a102c8adad47075d1a1901fe8be2c375
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-21 21:22:44 +0900

    Title: 최단 Run Length 인코딩, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240321/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 240321/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -1,23 +1,44 @@
 import java.util.*;
 
 public class Main {
-
+    
     public static char[] a;
 
     public static void shift(){
-        char[] aa = new char[10];
+        char[] aa = new char[a.length];
         aa[0] = a[a.length-1];
         for(int i=1; i<a.length; i++)
             aa[i] = a[i-1];
         for(int i=0; i<a.length; i++)
             a[i] = aa[i];
-        //System.out.println(aa);
+        //System.out.println(a);
     }
 
     public static int count(){
-        int cnt = 2;
+        int cnt = 0;
+        int num = 1;
         for(int i=0; i<a.length-1; i++){
-            if(a[i] != a[i+1]) cnt += 2;
+            if(a[i] == a[i+1]){
+                num++;
+            }
+            /*else if(i == a.length-2 && a[i] == a[i+1]){
+                cnt += (num/10)+2;
+            }
+            else if(i == a.length-2 && a[i] != a[i+1]){
+                cnt += (num/10)+4;
+            }*/
+            else{
+                cnt += (num/10)+1+1;
+                num = 1;
+            }
+            
+        }
+        if(a[a.length-2] == a[a.length-1]){
+            cnt += (num/10)+1;
+        }
+        else{
+            cnt += (num/10)+1;
+            cnt += 2;
         }
         return cnt;
     }
@@ -25,29 +46,13 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        a= sc.next().toCharArray();
-        int len = a.length;
-        int min = 10000;
-        boolean bool = true;
-        char abc = a[0];
-        for(int i=1; i<len; i++){
-            if(abc != a[i]){
-                bool = false;
-                break;
-            }
-        }
-        if(!bool){
-            for(int i=1; i<=len; i++){
+        a = sc.next().toCharArray();
+        int min = 100000000;
+        for(int i=0; i<a.length; i++){
             shift();
             int cnt = count();
-            min = Math.min(cnt, min);
-            }
-            System.out.println(min);
+            min = Math.min(cnt+1, min);
         }
-        else{
-            System.out.println("3");
-        }
-        
-        
+        System.out.println(min);
     }
 }
