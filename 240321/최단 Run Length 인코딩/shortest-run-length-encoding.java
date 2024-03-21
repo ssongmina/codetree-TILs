080f0818bcb932985f362f697dfaf14b9aa1f35d
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-21 21:28:28 +0900

    Title: 최단 Run Length 인코딩, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240321/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 240321/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -5,42 +5,30 @@ public class Main {
     public static char[] a;
 
     public static void shift(){
-        char[] aa = new char[a.length];
-        aa[0] = a[a.length-1];
-        for(int i=1; i<a.length; i++)
-            aa[i] = a[i-1];
-        for(int i=0; i<a.length; i++)
-            a[i] = aa[i];
-        //System.out.println(a);
+        char tmp = a[a.length-1];
+        for(int i=a.length-1; i>0; i--)
+            a[i] = a[i-1];
+        a[0] = tmp;
     }
 
     public static int count(){
-        int cnt = 0;
-        int num = 1;
-        for(int i=0; i<a.length-1; i++){
-            if(a[i] == a[i+1]){
-                num++;
-            }
-            /*else if(i == a.length-2 && a[i] == a[i+1]){
-                cnt += (num/10)+2;
-            }
-            else if(i == a.length-2 && a[i] != a[i+1]){
-                cnt += (num/10)+4;
-            }*/
+        int cnt = 1;
+        char first = a[0];
+        String s = "";
+        for(int i=1; i<a.length; i++){
+            char next = a[i];
+            if(first == next) cnt++;
             else{
-                cnt += (num/10)+1+1;
-                num = 1;
+                s += first;
+                s += cnt;
+
+                first = next;
+                cnt = 1;
             }
-            
         }
-        if(a[a.length-2] == a[a.length-1]){
-            cnt += (num/10)+1;
-        }
-        else{
-            cnt += (num/10)+1;
-            cnt += 2;
-        }
-        return cnt;
+        s += first;
+        s += cnt;
+        return s.length();
     }
 
     public static void main(String[] args) {
@@ -51,7 +39,7 @@ public class Main {
         for(int i=0; i<a.length; i++){
             shift();
             int cnt = count();
-            min = Math.min(cnt+1, min);
+            min = Math.min(cnt, min);
         }
         System.out.println(min);
     }
