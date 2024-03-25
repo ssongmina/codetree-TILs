f667c26f8592f0d214d1d9aa728b9a1db1f7be32
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-25 10:17:14 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240325/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n,r,c;
+    public static int[][] arr;
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        r = sc.nextInt();
+        c = sc.nextInt();
+        arr = new int[n+1][n+1];
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
+                arr[i][j] = sc.nextInt();
+        // 상 하 좌 우 우선 순위
+        int[] dx = {-1, 1, 0, 0}; 
+        int[] dy = {0, 0, -1, 1};
+        boolean bool = true;
+        System.out.print(arr[r][c]+" ");
+        while(bool){
+            boolean change = false;
+            for(int i=0; i<4; i++){
+                int x = r + dx[i];
+                int y = c + dy[i];
+                if(arr[r][c] < arr[x][y]){
+                    r = x;
+                    c = y;
+                    System.out.print(arr[r][c]+" ");
+                    change = true;
+                    break;
+                }
+            }
+            if(change == false)
+                bool = false;
+            
+        }
+    }
+}
