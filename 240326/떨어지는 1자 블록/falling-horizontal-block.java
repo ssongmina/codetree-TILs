0387bfd6a13f5b5cf5b35aa8765890e172479cdb
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-03-26 12:31:38 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240326/떨어지는 1자 블록/falling-horizontal-block.java
+++ 240326/떨어지는 1자 블록/falling-horizontal-block.java
@@ -1,61 +1,54 @@
 import java.util.*;
 
 public class Main {
-    
+
     public static int n,m,k;
     public static int[][] arr;
 
-    /*public static int check(int row){
+    public static boolean find(int row){
         int cnt = 0;
+        int empty = 0;
         for(int i=k; i<=k+m-1; i++){
-            if(arr[row+1][i] == 1)  cnt++;
+            if(arr[row][i] == 1) cnt++;
+            if(arr[row+1][i] == 1) empty++;
         }
-        return cnt;
-    }*/
-
-    public static int find(){
-        for(int i=n; i>=1; i--){
-            int cnt = 0;
-            for(int j=k; j<=k+m-1; j++){
-                if(arr[i][j] == 0)  cnt++;
-            } 
-            if(cnt == m){
-                int have = 0;
-                for(int j=k; j<=k+m-1; j++){
-                    if(arr[i-1][j] == 1) have++;
-                }
-                if(have == 0)
-                    return i;
-                else
-                    continue;
-            }
-            
-        }
-        return -1;
-        
+        if(cnt == 0 && empty != 0)
+            return true;
+        else
+            return false;
     }
 
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+
+        // 입력
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        k = sc.nextInt(); // k열 부터 k_m-1열까지
+        k = sc.nextInt(); // k번 ~ k+m-1번
         arr = new int[n+1][n+1];
         for(int i=1; i<=n; i++)
             for(int j=1; j<=n; j++)
                 arr[i][j] = sc.nextInt();
 
-        int row = find();
-       // System.out.println(row);
-        for(int i=k; i<=k+m-1; i++){
-            arr[row][i] = 1;
+        // 넣을 수 있는 row 값 찾기
+        int row = -1;
+        for(int i=1; i<=n; i++){
+            boolean row1 = find(i);
+            if(row1){
+                row = i;
+                break;
+            }
         }
+        // 그 row에 값을 1로 변경
+        for(int i=k; i<=k+m-1; i++)
+            arr[row][i] = 1;
+        // 출력
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++)
                 System.out.print(arr[i][j]+" ");
             System.out.println();
         }
-
+        
     }
 }
