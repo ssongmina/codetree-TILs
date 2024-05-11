9734abf9775b232bf140306db5f453597825b370
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-05-11 22:09:38 +0900

    Title: 사다리 타기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240511/사다리 타기/ladder-game.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    int a, b;
+
+    public Pair(int a, int b) {
+        this.a = b;
+        this.b = a;
+    }
+
+    @Override
+    public int compareTo(Pair other) {
+        return Integer.compare(this.a, other.a);
+    }
+}
+
+
+public class Main {
+
+    public static int n,m;
+    public static int min = 100;
+    public static ArrayList<Pair> linear = new ArrayList<>();
+    public static int[] ans = new int[12];
+    public static int[] arr = new int[12];
+
+    public static void check(int cnt){
+        for(int i=1; i<=n; i++){
+            if(arr[i] != ans[i])
+                return ;
+        }
+        min = Math.min(min, cnt);
+        return ;
+    }
+
+    public static void change(int cnum){
+        int tmp = arr[cnum];
+        arr[cnum] = arr[cnum+1];
+        arr[cnum+1] = tmp;
+    }
+
+    public static void unchange(int cnum){
+        int tmp = arr[cnum];
+        arr[cnum] = arr[cnum+1];
+        arr[cnum+1] = tmp; 
+    }
+
+    public static void choose(int num, int cnt){
+        // 더 이상 진행 할 수 없기에 check() 후 종료.
+        if(num == (int)linear.size()){
+            check(cnt);
+            return ;
+        }
+        int k = linear.get(num).b;
+        change(k);
+        check(cnt+1);
+        choose(num+1, cnt+1);
+        unchange(k);
+    }
+    
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i=0; i<m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            linear.add(new Pair(a,b));
+        }
+
+        Collections.sort(linear);
+
+        for(int i=1; i<=n; i++){
+            ans[i] = i;
+            arr[i] = i;
+        }
+
+        // 정답 제작
+        for(int i=0; i<linear.size(); i++){
+            int a = linear.get(i).b;
+            int tmp = ans[a];
+            ans[a] = ans[a+1];
+            ans[a+1] = tmp; 
+        }
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            if(ans[i] == i)
+                count++;
+        }
+        
+        // 재귀 실행
+        int cnt = 0;
+        for(int i=0; i<n; i++)
+            choose(i, cnt);
+
+        // 최소 선분 수 출력
+        if(count == n)
+            System.out.println("0");
+        else
+            System.out.println(min);
+    }
+}
