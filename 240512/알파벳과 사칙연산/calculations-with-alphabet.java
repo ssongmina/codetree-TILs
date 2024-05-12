66ca517caebcae786bdf94952fa75c6f21cea7eb
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-05-12 17:01:29 +0900

    Title: 알파벳과 사칙연산, Time: 130ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240512/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+
+    public static int n,max;
+    public static String str;
+    public static char c = 'a';
+    public static int[] alphabet = new int[6];
+    public static char[] arr;
+
+    public static void check(){
+        // arr로 값 계산해서 max에 값 담기
+        char fir = arr[0];
+        int sum = alphabet[(int)(fir-'a')];
+        int cnt = 1;
+        for(int i=0; i<(n-1)/2; i++){
+            char a = arr[cnt+1];
+            switch(arr[cnt]){
+                case '+':
+                    sum += alphabet[(int)(a-'a')];
+                    break;
+                case '-':
+                    sum -= alphabet[(int)(a-'a')];
+                    break;
+                case '*':
+                    sum *= alphabet[(int)(a-'a')];
+                    break;
+            }
+            cnt += 2;
+        }
+        max = Math.max(max, sum);
+    }
+
+    public static void find(int num){
+        if((c-'a')+1 == num){
+            check();
+            return;
+        }
+        for(int i=1; i<=4; i++){
+            alphabet[num] = i;
+            find(num+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        str = sc.next();
+        n = str.length();
+        arr = new char[n];
+        for(int i=0; i<n; i++){
+            arr[i] = str.charAt(i);
+            if((int)arr[i] > c)
+                c = arr[i];
+        }
+
+        find(0);
+        System.out.println(max);
+    }
+}
