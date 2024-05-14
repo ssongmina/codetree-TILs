f4eb5fe98bc376d8d15be646e04a92b64b892bcf
ssongmina <115204391+ssongmina@users.noreply.github.com>
2024-05-14 21:58:11 +0900

    Title: xor 결과 최대 만들기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240514/xor 결과 최대 만들기/max-of-xor.java
+++ 240514/xor 결과 최대 만들기/max-of-xor.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 
     public static int n,m, max = 0;
+    public static ArrayList<Integer> input = new ArrayList<>();
     public static ArrayList<Integer> arr = new ArrayList<>();
 
     public static void choose(int num, int cnt){
@@ -16,7 +17,7 @@ public class Main {
                 return;
         }
 
-        arr.add(num);
+        arr.add(input.get(num-1));
         choose(num+1, cnt+1);
         arr.remove(arr.size()-1);
 
@@ -27,7 +28,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        
+        for(int i=0; i<n; i++){
+            int a = sc.nextInt();
+            input.add(a);
+        }
         choose(1, 0);
         if(n==m && m==1)
             System.out.println("0");
